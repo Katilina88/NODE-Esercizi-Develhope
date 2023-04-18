ea2765e35d5813a331576c27b0ff8dc56f3c5e86
Katilina88 <caterinalograsso@gmail.com>
2023-04-18 11:19:04 +0200

    esercizio fatto

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,39 @@
-const TODO: string = "start writing your Express API server here :)";
 
-console.log(TODO);
+import express from "express";
+import "express-async-errors";
+import morgan from "morgan"; 
+
+const dotenv = require('dotenv');
+dotenv.config(); 
+
+const app = express(); 
+const port = process.env.PORT; 
+
+app.use(morgan("dev"))
+app.use(express.json())
+type Planet = {
+    id: number,
+    name: string,
+  };
+
+  type Planets = Planet[];
+
+  let planets: Planets = [
+    {
+      id: 1,
+      name: "Earth",
+    },
+    {
+      id: 2,
+      name: "Mars",
+    },
+  ];
+
+app.get("/", (req, res)=> {
+    res.status(200).json(planets);
+    
+});
+
+app.listen(port, () => {
+    console.log(`example app running on port http://localhost:${port}`);
+}); 
