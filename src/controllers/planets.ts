0b7dd915321d79a245991e802386c5e47145dd33
Caterina Lo Grasso <118819535+Katilina88@users.noreply.github.com>
2023-05-04 12:20:36 +0200

    Update planets.ts

--- src/controllers/planets.ts
+++ src/controllers/planets.ts
@@ -20,11 +20,14 @@ const setupDb = async () => {
     DROP TABLE IF EXISTS planets;
     CREATE TABLE planets (
         id SERIAL NOT NULL PRIMARY KEY,
-        name TEXT NOT NULL
+        name TEXT NOT NULL,
+        image TEXT 
     );
-    INSERT INTO planets (name) VALUES ('Earth');
-    INSERT INTO planets (name) VALUES ('Mars');
-  `);
+     `);
+     
+      await db.none (`INSERT INTO planets (name) VALUES ('Earth')`);
+      await db.none (`INSERT INTO planets (name) VALUES ('Mars')`);
+ 
 };
 
 setupDb();
